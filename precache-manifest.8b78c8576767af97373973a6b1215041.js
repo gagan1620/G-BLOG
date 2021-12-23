d74640375a0a4bd9ec00db77eb8879eb3b4349cb
Gagan <bajajsons7479@gmail.com>
2021-12-23 23:34:55 +0530

    'deploy'

---
+++ precache-manifest.8b78c8576767af97373973a6b1215041.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cc191e95f019f8b603d",
+    "url": "/G-BLOG/css/app.cc11bbea.css"
+  },
+  {
+    "revision": "3ba50b0cd751adc47313f080336e4f90",
+    "url": "/G-BLOG/img/1.3ba50b0c.jpg"
+  },
+  {
+    "revision": "048d49ff3d50da154c0b5b0510b1378f",
+    "url": "/G-BLOG/img/background.048d49ff.png"
+  },
+  {
+    "revision": "abd940b507a70374012448349f033462",
+    "url": "/G-BLOG/img/beautiful-stories.abd940b5.jpg"
+  },
+  {
+    "revision": "2b6df2ee1a50cf8927443a277f125c06",
+    "url": "/G-BLOG/img/coding.2b6df2ee.jpg"
+  },
+  {
+    "revision": "c10671c4a7bae8679d849e6bf4b43bd4",
+    "url": "/G-BLOG/img/designed-for-everyone.c10671c4.jpg"
+  },
+  {
+    "revision": "053275905baf8d83fa8752f74e4e46af",
+    "url": "/G-BLOG/index.html"
+  },
+  {
+    "revision": "5c8e0bc27fbe55c898a0a1ba73e75d6f",
+    "url": "/G-BLOG/index.js"
+  },
+  {
+    "revision": "4cc191e95f019f8b603d",
+    "url": "/G-BLOG/js/app.0f2dff5a.js"
+  },
+  {
+    "revision": "8c435e0f3461283f4010",
+    "url": "/G-BLOG/js/chunk-vendors.7d114f34.js"
+  },
+  {
+    "revision": "707b7d485bd47db16f251e363948d421",
+    "url": "/G-BLOG/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/G-BLOG/robots.txt"
+  },
+  {
+    "revision": "43b24c5231268a415c6a434ea6cf1fb8",
+    "url": "/G-BLOG/sw.js"
+  }
+]);
